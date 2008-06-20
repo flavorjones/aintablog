5cc3a8c053986dbff9386ee3845fcdfaeb78d190
JD Huntington <jedediah@jdhuntington.com>
2008-06-19 22:16:25 -0700

    Quick and dirty theme support

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -2,6 +2,9 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+
+  before_filter :add_theme_path
+  
   include AuthenticatedSystem
   
   helper :all # include all helpers, all the time
@@ -11,4 +14,9 @@ class ApplicationController < ActionController::Base
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery :secret => '88b98c68052725f207bad5c1aec2355a'
+
+  def add_theme_path
+    self.prepend_view_path "#{RAILS_ROOT}/themes/#{SITE_SETTINGS['theme']}"
+  end
+  
 end

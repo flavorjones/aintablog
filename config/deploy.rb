71c456e644a44795cb32685bec702557e341e9e4
Mike Dalessio <mike@csa.net>
2008-10-29 03:46:19 -0400

    deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,3 +4,24 @@ namespace :cache do
     run "rm -rf #{current_path}/public/cache"
   end
 end
+
+set :keep_releases, 5
+set :application, "aintablog"
+set :repository, "ssh://dalessio.csa.net/var/cache/git/daless.io/aintablog"
+set :scm, :git
+set :git_shallow_clone, 1
+set :user, "mike"
+set :password, "the sounds of science"
+set :runner, "mike"
+set :deploy_to, "/home/mike/#{application}"
+set :deploy_via, :remote_cache
+set :repository_cache, "/home/mike/cache/#{application}"
+
+ssh_options[:paranoid] = false
+
+task :production do
+  role :web, "slice1.daless.io"
+  role :app, "slice1.daless.io", :mongrel => true
+  role :db, "slice1.daless.io", :primary => true
+  set :rails_env, "production"
+end
